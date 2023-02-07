b0159d5d5ae386063a45d805536b7b952487ac21
Farrel Muhammad <farrelf11@gmail.com>
2023-02-07 16:01:53 +0700

    Layouting navbar and banner

---
+++ .history/src/pages/Home_20230207155640.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            asdada
+            <Banner />
+        </>
+    )
+}
+
+export default Home
---
+++ .history/src/pages/Home_20230207155751.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            {/* <Banner /> */}
+<div>Cobas</div>
+        </>
+    )
+}
+
+export default Home
